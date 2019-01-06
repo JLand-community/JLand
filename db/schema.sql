a2a2d6893d4fadb0d0e97afe438a7184c1f6940d
Yana <yana.lysenkova@gmail.com>
2019-01-06 12:37:04 +0200

    refactor table creation

--- db/schema.sql
+++ db/schema.sql
@@ -12,82 +12,103 @@ CREATE DATABASE jland
 CREATE SCHEMA jland_site
   AUTHORIZATION admin;
 
+-- create sequences
+create sequence conference_id_seq;
+
+create sequence event_id_seq;
+
+create sequence user_id_seq;
+
+create sequence presentation_id_seq;
+
+create sequence presentation_plan_id_seq;
+
+create sequence participant_id_seq;
+
 -- create tables
-create table if not exists jland_site.conference
+create table conference
 (
-	id serial
+	id          serial not null
 		constraint conference_id_pkey
-			primary key,
-			description varchar,
-	date date,
-	address varchar(255)
-)
-;
+		primary key,
+	description varchar,
+	date        date,
+	address     varchar(255)
+);
 
-create table if not exists jland_site.event
+create table event
 (
-  id serial
+	id            serial  not null
 		constraint event_id_pkey
-			primary key,
-			conference_id integer not null
+		primary key,
+	conference_id integer not null
 		constraint event_conference_id_fkey
-			references jland_site.conference,
-  name varchar(255),
-  type varchar(255),
-  start_time time,
-  end_time time,
-  order integer
-)
-;
+		references conference,
+	name          varchar(255),
+	type          varchar(255),
+	start_time    time,
+	end_time      time,
+	event_order   integer
+);
 
-create table if not exists jland_site.presentation
+create index event_conference_id_idx
+	on event (conference_id);
+
+create table "user"
 (
-	id serial
+	id         serial not null
+		constraint user_id_pkey
+		primary key,
+	first_name varchar(255),
+	last_name  varchar(255),
+	photo      bytea
+);
+
+create table presentation
+(
+	id                 serial  not null
 		constraint presentation_id_pkey
-			primary key,
-	event_id integer not null
+		primary key,
+	event_id           integer not null
 		constraint presentation_presentation_id_fkey
-			references jland_site.event,
-	name varchar(255),
-	user_id integer,
-	constraint presentation_user_id_fkey
-			references jland_site.user,
+		references event,
+	name               varchar(255),
+	user_id            integer
+		constraint presentation_user_id_fkey
+		references "user",
 	presentation_order integer
-)
-;
+);
 
-create table if not exists jland_site.presentation_plan
+create index presentation_event_id_idx
+	on presentation (event_id);
+
+create table presentation_plan
 (
-	id serial
+	id              serial  not null
 		constraint presentation_plan_id_pkey
-			primary key,
+		primary key,
 	presentation_id integer not null
 		constraint presentation_plan_presentation_id_fkey
-			references jland_site.presentation,
-	point varchar(255),
-	item_order integer
-)
-;
+		references presentation,
+	point           varchar(255),
+	item_order      integer
+);
 
-create table if not exists jland_site.user
+create index presentation_plan_presentation_id_idx
+	on presentation_plan (presentation_id);
+
+create table participant
 (
-	id serial
-		constraint user_id_pkey
-			primary key,
-	first_name varchar(255),
-	last_name varchar(255),
-	photo bytea
-)
-;
+	id            serial                not null
+		constraint participant_id_pkey
+		primary key,
+	user_id       integer               not null
+		constraint participant_user_id_fkey
+		references "user",
+	conference_id integer               not null
+		constraint participant_conference_id_fkey
+		references conference,
+	speaker       boolean default false not null,
+	listener      boolean default false not null
+);
 
-create index if not exists presentation_event_id_idx
-	on jland_site.presentation (event_id)
-;
-
-create index if not exists event_conference_id_idx
-	on jland_site.event (conference_id)
-;
-
-create index if not exists presentation_plan_presentation_id_idx
-	on jland_site.presentation_plan (presentation_id)
-;
